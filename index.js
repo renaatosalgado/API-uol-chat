429c882f9f7d3b1989ed5dc1baf3ed78390667f4
renaatosalgado <renaato.salgado@gmail.com>
2022-01-27 11:00:53 -0300

    feat: post/participants without data validation

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-import dotenv from "dotenv";
 import express, { json } from "express";
-import { MongoClient } from "mongodb";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 dotenv.config();
 
 const server = express();
 server.use(json());
 server.use(cors());
 
-server.listen(5000, () => {
-    console.log("Rodando servidor na porta 5000.")
+server.listen(5000);
+
+server.post("/participants", async (req, res) => {
+  const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+  try {
+    await mongoClient.connect();
+    const participantName = await mongoClient
+      .db("uol-chat")
+      .collection("participants")
+      .insertOne({
+        name: req.body.name,
+        lastStatus: Date.now(),
+      });
+    res.status(200).send(participantName);
+    mongoClient.close();
+  } catch (error) {
+    res.send("deu problema aqui ó");
+    mongoClient.close();
+  }
 });
 
 
